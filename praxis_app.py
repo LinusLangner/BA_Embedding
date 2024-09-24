f8e9f11695640e546d89f82b97d95d5972595d81
LinusLangner <130920775+LinusLangner@users.noreply.github.com>
2024-09-24 12:38:01 +0200

    Updated Outputs

--- praxis_app.py
+++ praxis_app.py
@@ -452,7 +452,7 @@ st.markdown("""
 <div style="background-color: #f0f2f6; padding: 15px; border-radius: 5px; margin-top: 30px;">
     <h3 style="color: #0066cc;">Über diese Anwendung</h3>
     <p><strong>Titel der Bachelorarbeit:</strong> Innovationen durch Künstliche Intelligenz: Automatisierte Dokumentenverarbeitung in der Bekleidungsindustrie</p>
-    <p>Diese Anwendung demonstriert die praktische Umsetzung automatisierter Verarbeitung und Analyse von Rechnungen und Bestellungen unter Einsatz künstlicher Intelligenz und moderner Vertragsmanagement-Techniken.</p>
+    <p>Diese Anwendung demonstriert die praktische Umsetzung automatisierter Verarbeitung und Analyse von Rechnungen und Bestellungen unter Einsatz künstlicher Intelligenz.</p>
     <p><strong>Student:</strong> Linus Langner<br>
     <strong>Semester:</strong> 9. Semester BTM SS24<br>
     <strong>Matrikelnummer:</strong> 2557735</p>
